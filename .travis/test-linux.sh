34183a214cc4f40a196b16112677f44f06992a12
Andrew Bradley <cspotcode@gmail.com>
2018-10-25 08:42:27 -0400

    ci fixes

--- .travis/test-linux.sh
+++ .travis/test-linux.sh
@@ -5,7 +5,5 @@ mkdir pwsh
 pushd pwsh
 wget -qO- https://github.com/PowerShell/PowerShell/releases/download/v6.2.0-preview.1/powershell-6.2.0-preview.1-linux-x64.tar.gz -o - | tar -xvz
 popd
-mkdir local-bin
-ln -s ./pwsh/pwsh ./local-bin/pwsh 
-export PATH="$PWD/local-bin:$PATH"
+ln -s ./pwsh/pwsh ~/bin/pwsh
 pwsh -noprofile ./scripts/build.ps1 -compile -package -testLinux

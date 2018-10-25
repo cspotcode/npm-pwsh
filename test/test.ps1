51b56313fbb8862224946e29cfdd4b4a3a9b3d4e
Andrew Bradley <cspotcode@gmail.com>
2018-10-25 15:49:14 -0400

    ci fixes

--- test/test.ps1
+++ test/test.ps1
@@ -1,3 +1,4 @@
+write-host 'test.ps1: Start'
 $ErrorActionPreference = 'Stop'
 
 # For some reason with -noprofile I have to Get-command to trigger loading
@@ -9,4 +10,6 @@ Get-Command Get-ChildItem > $null
 Get-Command Add-Member > $null
 import-module pester
 
+write-host 'test.ps1: invoking pester'
 invoke-pester -verbose -enableexit
+write-host 'test.ps1: done'
